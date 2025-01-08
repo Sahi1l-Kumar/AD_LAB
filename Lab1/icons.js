81d09718d1edb22f5544ed2951027548c45c68f1
Sahi1l-Kumar <2230189@kiit.ac.in>
2025-01-08 12:35:23 +0530

    Lab 1 completed

---
+++ Lab1/icons.js
@@ -0,0 +1,17 @@
+export const skillIcons = {
+  HTML: "devicon-html5-plain colored",
+  CSS: "devicon-css3-plain colored",
+  JavaScript: "devicon-javascript-plain colored",
+  TypeScript: "devicon-typescript-plain colored",
+  React: "devicon-react-original colored",
+  "Node.js": "devicon-nodejs-plain colored",
+  Python: "devicon-python-plain colored",
+  AWS: "devicon-amazonwebservices-plain-wordmark colored",
+  Docker: "devicon-docker-plain colored",
+  Git: "devicon-git-plain colored",
+  SQL: "devicon-mysql-plain colored",
+  Nextjs: "devicon-nextjs-original colored",
+  Tailwindcss: "devicon-tailwindcss-plain colored",
+  Websockets: "fa-solid fa-network-wired",
+  GraphQL: "devicon-graphql-plain colored",
+};
