ac44b5dea6c70dfe0bce8d71aaf85eba328d4e00
Sahi1l-Kumar <2230189@kiit.ac.in>
2025-02-04 12:12:32 +0530

    formatting

--- Lab3/app.py
+++ Lab3/app.py
@@ -8,7 +8,6 @@ from flask_cors import CORS
 app = Flask(__name__, static_folder='styles', template_folder='.')
 CORS(app, origins=["http://localhost:5000/"])
 
-# Load the trained model and scaler
 model = joblib.load('models/linear_model.pkl')
 scaler = joblib.load('models/scaler.pkl')
 
@@ -19,20 +18,20 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get input data from request
+        
         data = request.json
         stock_symbol = data['Symbol']
         prediction_days = int(data['Days'])
 
-        # Placeholder values for demo purposes
-        # You might need to actually fetch data based on the stock symbol
+        
+        
         open_price, high_price, low_price, volume = 1000, 1050, 980, 2000000
 
-        # Prepare input for model
+        
         input_features = np.array([[open_price, high_price, low_price, volume]])
         input_scaled = scaler.transform(input_features)
 
-        # Predict stock price
+        
         predicted_price = model.predict(input_scaled)[0]
 
         return jsonify({
